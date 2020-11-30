77ff0392b4b6c7bc02fa489f20996a7bc4991ad6
lijidong <lijidong@tal.com>
2020-11-30 19:28:31 +0800

    add the new model files

---
+++ models/ATAE_LSTM.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+from torch.nn.modules import Module
+from torch.nn.modules import Embedding,LSTM,Linear,CrossEntropyLoss
+
+class Attention(Module):
+    def __init__(self,input):
+        super(Attention, self).__init__()
+
+
+class ATAELSTM(Module):
+
+    def __init__(self,input_size,embed_size, batch_size, hidden_size,embedding=None):
+        super(ATAELSTM, self).__init__()
+        self.embed_size = embed_size
+        if embedding is not None:
+            self.embeding = Embedding.from_pretrained(embedding,padding_idx=0)
+        else:
+            self.embeding = Embedding(input_size,embed_size,padding_idx=0)
+
+        self.rnn = LSTM(input_size=embed_size,hidden_size=hidden_size,bidirectional=True,batch_first=True,num_layers=1)
+        self.rnn = LSTM()
+        self.batch_size = batch_size
+        self.hidden_size = hidden_size
+
+    def forward(self,x):
+        pass
+
+
+
