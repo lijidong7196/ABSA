27e358cd23a3bcc50a9ee48cb9d697c8ddbefabe
lijidong <lijidong@tal.com>
2020-12-02 20:46:36 +0800

    update code

--- dataset/train.py
+++ dataset/train.py
@@ -1,1 +1,4 @@
 import torch
+
+a = torch.tensor([[1,2],[3,5]],dtype=torch.int64)
+print(a.size()[-1])
--- models/ATAE_LSTM.py
+++ models/ATAE_LSTM.py
@@ -4,9 +4,20 @@ from torch.nn.modules import Module
 from torch.nn.modules import Embedding,LSTM,Linear,CrossEntropyLoss
 
 class Attention(Module):
-    def __init__(self,input):
+    def __init__(self,hidden_dim_size,apsect_dim_size):
+        self.hidden_dim_size = hidden_dim_size
+        self.apsect_dim_size = apsect_dim_size
+        self.W_h = Linear(self.hidden_dim_size,self.hidden_dim_size)
+        self.W_v = Linear(self.apsect_dim_size,self.apsect_dim_size)
+        self.w = Linear(1, self.hidden_dim_size + self.apsect_dim_size)
         super(Attention, self).__init__()
 
+    def forward(self,hidden,aspect):
+        M = torch.tanh(torch.cat([self.W_h(hidden),self.W_v(aspect)],dim=-1))
+        # (1, N) * (d +d_a , N)
+        alpha = torch.softmax(self.w)
+        torch.cat([])
+        pass
 
 class ATAELSTM(Module):
 
@@ -18,7 +29,7 @@ class ATAELSTM(Module):
             self.embeding = Embedding.from_pretrained(embedding,padding_idx=0)
         else:
             self.embeding = Embedding(input_size,embed_size,padding_idx=0)
-
+        # (batch size, N, embedding size)
         self.apect_embeding = Embedding(aspect_size,embed_size)
         self.rnn = LSTM(input_size=embed_size,
                         hidden_size=hidden_size,
@@ -29,11 +40,9 @@ class ATAELSTM(Module):
     def forward(self,input,term):
         x = self.embeding(input)
         aspect = self.apect_embeding(term)
-        torch.cat([x,aspect],dim=-1)
-
-
+        feature = torch.cat([x,aspect],dim=-1)
+        _,() = self.rnn(feature)
     def param_init(self):
         pass
-
 
 
