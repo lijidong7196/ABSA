eb6e95f4066d1d4f46870a12e36226ebc106af81
lijidong <lijidong@tal.com>
2020-12-08 18:00:26 +0800

    implement attention

--- models/ATAE_LSTM.py
+++ models/ATAE_LSTM.py
@@ -5,19 +5,31 @@ from torch.nn.modules import Embedding,LSTM,Linear,CrossEntropyLoss
 
 class Attention(Module):
     def __init__(self,hidden_dim_size,apsect_dim_size):
+        super(Attention, self).__init__()
         self.hidden_dim_size = hidden_dim_size
         self.apsect_dim_size = apsect_dim_size
+        #(d,d)
         self.W_h = Linear(self.hidden_dim_size,self.hidden_dim_size)
+        #(d_a,d_a)
         self.W_v = Linear(self.apsect_dim_size,self.apsect_dim_size)
-        self.w = Linear(1, self.hidden_dim_size + self.apsect_dim_size)
-        super(Attention, self).__init__()
+        #(1, d_a)
+        self.w = Linear(self.hidden_dim_size + self.apsect_dim_size,1)
 
     def forward(self,hidden,aspect):
-        M = torch.tanh(torch.cat([self.W_h(hidden),self.W_v(aspect)],dim=-1))
-        # (1, N) * (d +d_a , N)
-        alpha = torch.softmax(self.w)
-        torch.cat([])
-        pass
+        # transform the aspect embedding into shape of (batch, 1, v_a)
+        transfored_v = self.W_v(aspect)
+        # repeated the the transfored aspect embedding for N times to generate tensor with shape of (batch, N, v_a)
+        repeated_v = transfored_v.repeat(1,hidden.size(1),1)
+        # now, we concat transformed hidden and aspect embedding vector to generate M tensor with shape of (batch, N,d+d_a)
+        M = torch.tanh(torch.cat([self.W_h(hidden),repeated_v],dim=-1))
+        # then, we firstly transformed M with the matrix w, then pass it into softmax function
+        # to generate shape of (batch, N, 1)
+        alpha = torch.softmax(self.w(M).squeeze(-1),dim=-1).unsqueeze(-1)
+        # we get the weight for each hidden, then, we attach it to hidden state and generate the shape of (batch, 1, v)
+        r = torch.bmm(alpha.permute(0,2,1),hidden)
+
+        return  r
+
 
 class ATAELSTM(Module):
 
@@ -36,13 +48,18 @@ class ATAELSTM(Module):
                         bidirectional=True,
                         batch_first=True,
                         num_layers=1)
+        self.att = Attention(hidden_size,aspect_size)
 
     def forward(self,input,term):
         x = self.embeding(input)
         aspect = self.apect_embeding(term)
-        feature = torch.cat([x,aspect],dim=-1)
-        _,() = self.rnn(feature)
+        output,_ = self.rnn(x)
+        r = self.att(output,aspect)
+
     def param_init(self):
         pass
+
+
+
 
 
