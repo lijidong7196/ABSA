48fa0a994dd9ffd348360401a2138d9cfc67f0a2
lijidong <719629920@qq.com>
2020-12-28 22:07:48 +0800

    update code

--- models/ATAE_LSTM.py
+++ models/ATAE_LSTM.py
@@ -2,6 +2,14 @@ import torch
 import torch.nn as nn
 from torch.nn.modules import Module
 from torch.nn.modules import Embedding,LSTM,Linear,CrossEntropyLoss
+import os
+import random
+import numpy as np
+import torch
+from collections import namedtuple
+from torch.utils.data import DataLoader,Dataset
+import torch.optim as optim
+from utils.data_hepler import ABSADataset
 
 class Attention(Module):
     def __init__(self,hidden_dim_size,apsect_dim_size):
@@ -14,6 +22,10 @@ class Attention(Module):
         self.W_v = Linear(self.apsect_dim_size,self.apsect_dim_size)
         #(1, d_a)
         self.w = Linear(self.hidden_dim_size + self.apsect_dim_size,1)
+        # to define projection parameters for W_p and W_x
+        self.W_p = Linear(self.hidden_dim_size,self.hidden_dim_size)
+        self.W_x = Linear(self.hidden_dim_size,self.hidden_dim_size)
+
 
     def forward(self,hidden,aspect):
         # transform the aspect embedding into shape of (batch, 1, v_a)
@@ -27,18 +39,21 @@ class Attention(Module):
         alpha = torch.softmax(self.w(M).squeeze(-1),dim=-1).unsqueeze(-1)
         # we get the weight for each hidden, then, we attach it to hidden state and generate the shape of (batch, 1, v)
         r = torch.bmm(alpha.permute(0,2,1),hidden)
-
-        return  r
-
+        # we get the weight for each hidden units and Then we attach them to
+        output = torch.tanh(self.W_p(r) + self.W_x(hidden[:,-1,:]))
+        return  output
 
 class ATAELSTM(Module):
 
-    def __init__(self,input_size,embed_size, hidden_size,aspect_size,embedding=None):
+    def __init__(self,input_size,embed_size, hidden_size,aspect_size,num_class,embedding=None):
         super(ATAELSTM, self).__init__()
         self.embed_size = embed_size
         self.aspect_size = aspect_size
+        self.num_class = num_class
+        # emeddding 
         if embedding is not None:
             self.embeding = Embedding.from_pretrained(embedding,padding_idx=0)
+            self.embeding.weight.requires_grad = False
         else:
             self.embeding = Embedding(input_size,embed_size,padding_idx=0)
         # (batch size, N, embedding size)
@@ -49,13 +64,155 @@ class ATAELSTM(Module):
                         batch_first=True,
                         num_layers=1)
         self.att = Attention(hidden_size,aspect_size)
-        self.W_p = Linear()
+        self.fc = Linear(hidden_size,num_class,bias=True)
+
     def forward(self,input,term):
         x = self.embeding(input)
         aspect = self.apect_embeding(term)
         output,_ = self.rnn(x)
-        r = self.att(output,aspect)
+        # output the final attention represent
+        att_present = self.att(output,aspect).squeeze(1)
+        output = torch.softmax(att_present,dim=-1)
+        return output
 
-    def param_init(self):
-        pass
 
+class SpanMltEvaluator():
+
+    def __init__(self, **kwargs):
+        self.cfg = self.parse_args(**kwargs)
+
+        # setup GPU device if available
+        self.cuda = torch.cuda.is_available()
+        self.device = torch.device('cuda:0' if self.cuda else 'cpu')
+
+        # setup model
+        self.net = ATAELSTM(50000,300,300,300,20)
+        #self.net.initialize_weights()
+        self.net = self.net.to(self.device)
+
+        # setup criterion
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adagrad(
+            self.net.parameters(), lr=self.cfg.lr,weight_decay=self.cfg.weight_decay)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(
+            self.optimizer, step_size=10, gamma=0.1)
+
+    def parse_args(self, **kwargs):
+        # default parameters
+        cfg = {
+            # basic parameters
+            'out_scale': 0.001,
+            'norm_mean': [0.485, 0.456, 0.406],
+            'norm_std': [0.229, 0.224, 0.225],
+            'batch_size': 25,
+            'epoch': 50,
+            'log_interval': 10,
+            'lr': 0.01,
+            'num_workers': 8,
+            'weight_decay':0.001
+        }
+
+        for key, val in kwargs.items():
+            if key in cfg:
+                cfg.update({key: val})
+        return namedtuple('Config', cfg.keys())(**cfg)
+
+    def test(self, test_dir="Datasets/cartoon_set_test/"):
+        self.net.eval()
+        # test_dir = "Datasets/celeba_test/"
+        net_path = 'B2/model/model.pth'
+
+
+        test_data = CELEBAData(
+            data_dir=test_dir, transform=test_transform)
+        valid_loader = DataLoader(dataset=test_data, batch_size=1)
+        self.net.load_state_dict(torch.load(
+            net_path, map_location=lambda storage, loc: storage))
+        total_t = 0
+        correct_t = 0
+
+        for i, data in enumerate(valid_loader):
+            # forward
+            inputs, labels = data
+            inputs = inputs.to(self.device, non_blocking=self.cuda)
+            labels = labels.to(self.device, non_blocking=self.cuda)
+            outputs = self.net(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total_t += 1
+            correct_t += (predicted ==
+                          labels).squeeze().sum().cpu().numpy()
+        acc = correct_t/total_t
+        print("Eye_color识别率为%.2f" % (acc))
+
+    @ torch.enable_grad()
+    def train(self, train_dir, save_dir='save_models'):
+
+        self.net.train()
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+
+        train_loader = DataLoader(
+            dataset=train_data,
+            batch_size=self.cfg.batch_size,
+            num_workers=self.cfg.num_workers,
+            pin_memory=self.cuda,
+            shuffle=True)
+        max_acc = 0
+        for epoch in range(self.cfg.epoch):
+
+            loss_train = 0.
+            for i, data in enumerate(train_loader):
+
+                # forward
+                inputs, labels = data
+                inputs = inputs.to(self.device, non_blocking=self.cuda)
+                labels = labels.to(self.device, non_blocking=self.cuda)
+                outputs = self.net(inputs)
+
+                # backward
+                self.optimizer.zero_grad()
+                loss = self.criterion(outputs, labels)
+                loss.backward()
+
+                # update weights
+                self.optimizer.step()
+
+                loss_train += loss.item()
+                if (i+1) % self.cfg.log_interval == 0:
+                    loss_mean = loss_train / self.cfg.log_interval
+                    print("Training:Epoch[{:0>3}/{:0>3}] Iteration[{:0>3}/{:0>3}] Loss: {:.4f} ".format(
+                        epoch+1, self.cfg.epoch, i+1, len(train_loader), loss_mean))
+                    loss_train = 0.
+            test_dir = "Datasets/cartoon_set_test/"
+
+            test_data = CELEBAData(
+                data_dir=test_dir, transform=test_transform)
+            valid_loader = DataLoader(dataset=test_data, batch_size=1)
+            total_t = 0
+            correct_t = 0
+
+            for i, data in enumerate(valid_loader):
+                # forward
+                inputs, labels = data
+                inputs = inputs.to(self.device, non_blocking=self.cuda)
+                labels = labels.to(self.device, non_blocking=self.cuda)
+                outputs = self.net(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total_t += 1
+                correct_t += (predicted ==
+                              labels).squeeze().sum().cpu().numpy()
+            acc = correct_t/total_t
+            print("识别率为%.2f" % (acc))
+
+            self.scheduler.step()
+            if max_acc < acc:
+                max_acc = acc
+                net_path = os.path.join(
+                    save_dir, 'model.pth')
+                torch.save(self.net.state_dict(), net_path)
+
+
+if __name__ == "__main__":
+    eye_color = Eye_color()
+    eye_color.train("../Datasets/cartoon_set/")
--- utils/data_hepler.py
+++ utils/data_hepler.py
@@ -2,6 +2,9 @@ import json
 import pandas as pd
 from torch.utils.data import Dataset,DataLoader
 from tqdm import tqdm
+import pickle
+import numpy as np
+import os
 
 def json_parser(data_path):
     with open(data_path, 'r') as f:
@@ -18,28 +21,138 @@ def json_parser(data_path):
     df = pd.DataFrame({'id':id,'sentence':sentences,'term':terms,'polarity':polarities})
     return df
 
-def build_vocab(sentences,tokenizer,max_size,min_freq):
-    vocab_dict = {}
-    raw_data = sentences['sentence'].to_list()
-    for sent in tqdm(raw_data):
-        sent.strip()
-        for word in tokenizer(sent):
-            vocab_dict[word] = vocab_dict.get(word, 0) + 1
-    vocab_list = sorted([_ for _ in vocab_dict.items() if _[1] >= min_freq], key=lambda x:x[1],reverse=True)[:max_size]
-    vocab_dict = {word_count[0]: idx for idx, word_count in enumerate(vocab_list)}
-    vocab_dict.update({'UNK':len(vocab_dict),'PAD':len(vocab_dict) + 1})
-    return vocab_dict
+def build_tokenizer(fnames, max_seq_len, dat_fname):
+    if os.path.exists(dat_fname):
+        print('loading tokenizer:', dat_fname)
+        tokenizer = pickle.load(open(dat_fname, 'rb'))
+    else:
+        text = ''
+        for fname in fnames:
+            fin = open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+            lines = fin.readlines()
+            fin.close()
+            for i in range(0, len(lines), 3):
+                text_left, _, text_right = [s.lower().strip() for s in lines[i].partition("$T$")]
+                aspect = lines[i + 1].lower().strip()
+                text_raw = text_left + " " + aspect + " " + text_right
+                text += text_raw + " "
+
+        tokenizer = Tokenizer(max_seq_len)
+        tokenizer.fit_on_text(text)
+        pickle.dump(tokenizer, open(dat_fname, 'wb'))
+    return tokenizer
+
+def _load_word_vec(path, word2idx=None, embed_dim=300):
+    fin = open(path, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    word_vec = {}
+    for line in fin:
+        tokens = line.rstrip().split()
+        word, vec = ' '.join(tokens[:-embed_dim]), tokens[-embed_dim:]
+        if word in word2idx.keys():
+            word_vec[word] = np.asarray(vec, dtype='float32')
+    return word_vec
+
+
+def build_embedding_matrix(word2idx, embed_dim, dat_fname):
+    if os.path.exists(dat_fname):
+        print('loading embedding_matrix:', dat_fname)
+        embedding_matrix = pickle.load(open(dat_fname, 'rb'))
+    else:
+        print('loading word vectors...')
+        embedding_matrix = np.zeros((len(word2idx) + 2, embed_dim))  # idx 0 and len(word2idx)+1 are all-zeros
+        fname = './glove.twitter.27B/glove.twitter.27B.' + str(embed_dim) + 'd.txt' \
+            if embed_dim != 300 else './glove.42B.300d.txt'
+        word_vec = _load_word_vec(fname, word2idx=word2idx, embed_dim=embed_dim)
+        print('building embedding_matrix:', dat_fname)
+        for word, i in word2idx.items():
+            vec = word_vec.get(word)
+            if vec is not None:
+                # words not found in embedding index will be all-zeros.
+                embedding_matrix[i] = vec
+        pickle.dump(embedding_matrix, open(dat_fname, 'wb'))
+    return embedding_matrix
+
+def pad_and_truncate(sequence, maxlen, dtype='int64', padding='post', truncating='post', value=0):
+    x = (np.ones(maxlen) * value).astype(dtype)
+    if truncating == 'pre':
+        trunc = sequence[-maxlen:]
+    else:
+        trunc = sequence[:maxlen]
+    trunc = np.asarray(trunc, dtype=dtype)
+    if padding == 'post':
+        x[:len(trunc)] = trunc
+    else:
+        x[-len(trunc):] = trunc
+    return x
+
+
+class Tokenizer(object):
+    def __init__(self, max_seq_len, lower=True):
+        self.lower = lower
+        self.max_seq_len = max_seq_len
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 1
+
+    def fit_on_text(self, text):
+        if self.lower:
+            text = text.lower()
+        words = text.split()
+        for word in words:
+            if word not in self.word2idx:
+                self.word2idx[word] = self.idx
+                self.idx2word[self.idx] = word
+                self.idx += 1
+
+    def text_to_sequence(self, text, reverse=False, padding='post', truncating='post'):
+        if self.lower:
+            text = text.lower()
+        words = text.split()
+        unknownidx = len(self.word2idx)+1
+        sequence = [self.word2idx[w] if w in self.word2idx else unknownidx for w in words]
+        if len(sequence) == 0:
+            sequence = [0]
+        if reverse:
+            sequence = sequence[::-1]
+        return pad_and_truncate(sequence, self.max_seq_len, padding=padding, truncating=truncating)
 
 
 class ABSADataset(Dataset):
-    def __init__(self,data,tokenizer):
-        pass
+    def __init__(self,filename,tokenizer):
+        file = open(filename,'r',encoding='utf8',newline='\n',errors='ignore')
+        lines = file.readlines()
+        self.data = []
+        self.asp2idx = {}
+        for i in range(0,len(lines),3):
+            text_left, _, text_right = [s.lower().strip() for s in lines[i].partition('$T$')]
+            aspect = lines[i + 1].lower().strip()
+            polarity = lines[i + 2].strip()
+            #build text indices
+            # build vocabulary
+            full_text = text_left + ' ' + aspect + ' ' + text_right
+            tokenizer.fit_on_text(full_text)
+            text_indices = tokenizer.text_to_sequence(full_text)
+            polarity = int(polarity) + 1
+            data = {'text':text_indices,
+                    'aspect':aspect,
+                    'polarity':polarity}
+            if aspect not in self.asp2idx:
+                self.asp2idx[aspect] = len(aspect)
+
+            self.data.append(data)
+        for item in self.data:
+            item['aspect'] = self.asp2idx[item['aspect']]
 
     def __getitem__(self, item):
-        pass
+        return self.data[item]
 
     def __len__(self):
-        pass
+        return len(self.data)
+
+if __name__ == '__main__':
+    tokenizer = Tokenizer(30)
+    train = ABSADataset('../dataset/data/14lap/Laptops_Train.xml.seg',tokenizer)
+    dataset = DataLoader(train,batch_size=25, shuffle=True)
 
 
 
